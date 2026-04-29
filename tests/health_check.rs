e2090f1606fc69ec76d65357a98e6958b4b7648b
alex <pisonalex@gmail.com>
2026-04-29 23:18:28 +0200

    test: server used during testing running on OS assigned port

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,14 +1,15 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
-    // arrange
-    spawn_app();
-
-    // reqwest to perform HTTP requests against the application
+    // Arrange
+    let address = spawn_app();
+    // reqwest client to perform HTTP requests against the application
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(format!("{}/health_check", address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -18,8 +19,13 @@ async fn health_check_works() {
 }
 
 // Launch the application in the background ~somehow~
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    // Retrieve the port assigned by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     // Launch the server as a background task
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
