0a8197f5ab0d8d46101e0f9a8932773fa5eb4e90
alex <pisonalex@gmail.com>
2026-04-30 11:59:48 +0200

    feat: bash script to init postgres database

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
-            .route("/subscribtions", web::post().to(subscribe))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
