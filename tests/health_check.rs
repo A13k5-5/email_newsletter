62f3992d7b8c7f502efb459c8a998b72421b79a3
alex <pisonalex@gmail.com>
2026-05-02 23:28:40 +0200

    config: 'local' and 'production' configuration files

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,7 @@
 use once_cell::sync::Lazy;
+use secrecy::ExposeSecret;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
-use secrecy::ExposeSecret;
 use uuid::Uuid;
 use zero2prod::configuration::{DatabaseSettings, get_configuration};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -46,9 +46,10 @@ async fn spawn_app() -> TestApp {
 }
 
 async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    let mut connection = PgConnection::connect(&config.connection_string_without_db().expose_secret())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let mut connection =
+        PgConnection::connect(&config.connection_string_without_db().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres.");
 
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
