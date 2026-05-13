7c72cf09221dbe425487f71cf9a139524151d9da
alex <pisonalex@gmail.com>
2026-05-13 12:53:11 +0100

    feat: get_confirmed_subscribers method

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,7 +1,31 @@
-use actix_web::{HttpResponse, web};
+use crate::domain::SubscriberEmail;
+use crate::email_client::EmailClient;
+use crate::routes::error_chain_fmt;
+use actix_web::http::StatusCode;
+use actix_web::{HttpResponse, ResponseError, web};
+use anyhow::Context;
+use sqlx::PgPool;
+use std::fmt::{Debug, Formatter};
 
-pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn publish_newsletter(
+    pool: web::Data<PgPool>,
+    body: web::Json<BodyData>,
+    email_client: web::Data<EmailClient>,
+) -> Result<HttpResponse, PublishError> {
+    let subscribers = get_confirmed_subscribers(&pool).await?;
+    for subscriber in subscribers {
+        email_client
+            .send_email(
+                subscriber.email,
+                &body.title,
+                &body.content.html,
+                &body.content.text,
+            )
+            .await
+            .with_context(|| format!("Failed to send newsletter issue to {}", subscriber.email))?;
+    }
+
+    Ok(HttpResponse::Ok().finish())
 }
 
 #[derive(serde::Deserialize)]
@@ -14,4 +38,64 @@ pub struct BodyData {
 pub struct Content {
     text: String,
     html: String,
+}
+
+struct ConfirmedSubscriber {
+    email: SubscriberEmail,
+}
+
+#[tracing::instrument(name = "Get confirmed subscribers.", skip(db_pool))]
+async fn get_confirmed_subscribers(
+    db_pool: &PgPool,
+) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
+    struct Row {
+        email: String,
+    }
+    let rows = sqlx::query_as!(
+        Row,
+        r#"
+    SELECT email
+    FROM subscriptions
+    WHERE status = 'confirmed'
+"#
+    )
+    .fetch_all(db_pool)
+    .await?;
+
+    // Map into the domain type
+    let confirmed_subscribers = rows
+        .into_iter()
+        .filter_map(|r| match SubscriberEmail::parse(r.email) {
+            Ok(email) => Some(ConfirmedSubscriber { email }),
+            Err(error) => {
+                tracing::warn!(
+                    "A confirmed subscriber is using an invalid email address.\n{}",
+                    error
+                );
+                None
+            }
+        })
+        .collect();
+
+    Ok(confirmed_subscribers)
+}
+
+#[derive(thiserror::Error)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl Debug for PublishError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for PublishError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
 }
