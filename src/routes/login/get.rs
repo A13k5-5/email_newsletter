75bc24990bd3175816b2daef4f012c5d694d96a1
alex <pisonalex@gmail.com>
2026-05-17 20:58:21 +0100

    feat: html sanitisation

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -9,12 +9,11 @@ pub struct QueryParams {
 pub async fn login_form(query: web::Query<QueryParams>) -> HttpResponse {
     let error_html = match query.0.error {
         None => "".into(),
-        Some(error_message) => format!("<p><i>{error_message}</i></p>")
+        Some(error_message) => format!("<p><i>{}</i></p>", htmlescape::encode_minimal(&error_message))
     };
 
     HttpResponse::Ok()
         .content_type(ContentType::html())
-        // .body(include_str!("login.html"))
         .body(format!(
             r#"
 <!DOCTYPE html>
