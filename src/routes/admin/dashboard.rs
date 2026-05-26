cb51bf8deac8301c656eece6354cc6907ebc090a
alex <pisonalex@gmail.com>
2026-05-26 21:11:07 +0100

    config: sqlx prepare and style

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -1,6 +1,6 @@
 use actix_session::Session;
 use actix_web::http::header::ContentType;
-use actix_web::{web, HttpResponse};
+use actix_web::{HttpResponse, web};
 use anyhow::Context;
 use sqlx::PgPool;
 use uuid::Uuid;
@@ -13,7 +13,10 @@ where
     actix_web::error::ErrorInternalServerError(e)
 }
 
-pub async fn admin_dashboard(session: Session, db_pool: web::Data<PgPool>) -> Result<HttpResponse, actix_web::Error> {
+pub async fn admin_dashboard(
+    session: Session,
+    db_pool: web::Data<PgPool>,
+) -> Result<HttpResponse, actix_web::Error> {
     let username = if let Some(user_id) = session.get::<Uuid>("user_id").map_err(e500)? {
         get_username(user_id, &db_pool).await.map_err(e500)?
     } else {
@@ -21,7 +24,10 @@ pub async fn admin_dashboard(session: Session, db_pool: web::Data<PgPool>) -> Re
             .insert_header(("LOCATION", "/login"))
             .finish());
     };
-    Ok(HttpResponse::Ok().content_type(ContentType::html()).body(format!(r#"
+    Ok(HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(format!(
+            r#"
 <html lang="en">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
@@ -31,7 +37,8 @@ pub async fn admin_dashboard(session: Session, db_pool: web::Data<PgPool>) -> Re
     <h1>Welcome {username}!</h1>
 </body>
 </html>
-    "#)))
+    "#
+        )))
 }
 
 #[tracing::instrument(name = "Get username", skip(db_pool))]
@@ -41,6 +48,9 @@ async fn get_username(user_id: Uuid, db_pool: &PgPool) -> Result<String, anyhow:
         SELECT username FROM users WHERE user_id = $1
         "#,
         user_id
-    ).fetch_one(db_pool).await.context("Failed to fetch username from database")?;
+    )
+    .fetch_one(db_pool)
+    .await
+    .context("Failed to fetch username from database")?;
     Ok(row.username)
 }
